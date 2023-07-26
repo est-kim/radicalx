3086d0fb1b44df067ee14c77b68b4fd74359241d
Chrissy Fraser <plum5152@gmail.com>
2023-07-26 12:36:11 -0700

    fix: removed commented code & all avatar content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import Header from './components/Header/Header';
-// import AvatarCreatorComponent from './components/Avatar/AvatarCreator';
-// import { Avatar } from '@mui/material';
-// import LoadAvatar from './components/Avatar/LoadAvatar';
 
 function App() {
   return (
     <div>
       <Header />
-      {/* <AvatarCreatorComponent subdomain="radicalx"/> */}
-      {/* <LoadAvatar /> */}
     </div>
   );
 }
